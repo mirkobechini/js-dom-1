26a0de384a18cdf13289422cd924efb468e069da
mirkobechini <mirkobechini@gmail.com>
2025-07-22 18:52:14 +0200

    add variation changeLampStatus

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,24 @@
 //Functions
-function changeLampStatus(button){
-    if(button.innerText === "Accendi"){
+function changeLampStatus(button) {
+    if (button.innerText === "Accendi") {
+        lamp.src = "./assets/img/yellow_lamp.png"
+        button.innerText = "Spegni"
+    } else {
+        lamp.src = "./assets/img/white_lamp.png"
+        button.innerText = "Accendi"
+    }
+
+    /*
+
+    if(lamp.src.includes("white")){
         lamp.src = "./assets/img/yellow_lamp.png"
         button.innerText = "Spegni"
     }else{
         lamp.src= "./assets/img/white_lamp.png"
         button.innerText = "Accendi"
     }
+
+    */
 }
 
 //Page Elements
@@ -14,8 +26,8 @@ const light_switch = document.getElementById("light-switch")
 const lamp = document.getElementById("lamp")
 
 //Event Handler
-light_switch.addEventListener('click', function() {
-    
+light_switch.addEventListener('click', function () {
+
     changeLampStatus(light_switch);
 }
 )
